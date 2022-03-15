5ae68c07d5fd62ff3677ceaa634aa854df1bee7c
Justin Condrey <jlcondrey@ufl.edu>
2022-03-14 22:39:29 -0400

    second commit

--- src/Lab06.java
+++ src/Lab06.java
@@ -2,7 +2,7 @@ public class Lab06 {
     public static void main (String [] args) {
         int i = 2;
         int j = 4;
-        int k = i + j;
+        int k = j - i;
         System.out.println(k);
     }
 }
